7674581770c37acb5ddbf08fb339bc4a9e08ef77
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2019-12-16 15:37:04 -0500

    added component to display data

--- src/components/PermitsIssued.js
+++ src/components/PermitsIssued.js
@@ -4,6 +4,10 @@ function PermitsIssued(props) {
 
   return (
     <div>
+      <p>Demolition Permits Issued: {props.demolitionPermits}</p>
+      <p>Percent Change from Previous Year: {props.lastYearDemolitionRatio}</p>
+      <p>New Building Permits Issued: {props.newBuildingPermits}</p>
+      <p>Percent Change from Previous Year: {props.lastYearNewBuildingRatio}</p>
 
     </div>
   )
