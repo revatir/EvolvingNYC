eff731c56902be740d6ff22bf447c1222eb015e0
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2019-12-17 16:23:27 -0500

    added transition effect to menuItems

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -26,48 +26,48 @@ class Menu extends Component {
       marginRight: "6vw",
     }
 
-    // const menuItem = {
-    //   display: "flex",
-    //   justifyContent: "center",
-    //   alignItems: "center",
-    //   color: "inherit",
-    //   // backgroundColor: this.state.hover ? "rgba(214, 190, 175,.9)" : "#EFE7D2",
-    //   // fontSize: this.state.hover ? "22px" : "16px",
-    //   width: "15vw",
-    //   height: "10vw",
-    //   border: "2px solid black",
-    //   borderRadius: "15px",
-    //   boxShadow: "6px 6px black",
-    //   margin: ".3vw",
-    //   textDecoration: "none",
-    //   transition: ".5s",
-    // }
+    const menuItem = {
+      // display: "flex",
+      // justifyContent: "center",
+      // alignItems: "center",
+      // color: "inherit",
+      // // backgroundColor: this.state.hover ? "rgba(214, 190, 175,.9)" : "#EFE7D2",
+      // // fontSize: this.state.hover ? "22px" : "16px",
+      // width: "15vw",
+      // height: "10vw",
+      // border: "2px solid black",
+      // borderRadius: "15px",
+      // boxShadow: "6px 6px black",
+      // margin: ".3vw",
+      textDecoration: "none",
+      // transition: ".5s",
+    }
 
     return (
       <menu>
         {this.props.onHome &&
           <ul style={list}>
-            <Link to="Queens"
+            <Link style={menuItem} to="Queens"
               onClick={this.props.loadMenu}
               onMouseEnter={this.hoverEnter}
               onMouseLeave={this.hoverLeave}>
               <li id="Queens">Queens</li></Link>
-            <Link to="Brooklyn"
+            <Link style={menuItem} to="Brooklyn"
               onClick={this.props.loadMenu}
               onMouseEnter={this.hoverEnter}
               onMouseLeave={this.hoverLeave}>
               <li id="Brooklyn">Brooklyn</li></Link>
-            <Link to="Manhattan"
+            <Link style={menuItem} to="Manhattan"
               onClick={this.props.loadMenu}
               onMouseEnter={this.hoverEnter}
               onMouseLeave={this.hoverLeave}>
               <li id="Manhattan">Manhattan</li></Link>
-            <Link to="Bronx"
+            <Link style={menuItem} to="Bronx"
               onClick={this.props.loadMenu}
               onMouseEnter={this.hoverEnter}
               onMouseLeave={this.hoverLeave}>
               <li id="Bronx">Bronx</li></Link>
-            <Link to="StatenIsland"
+            <Link style={menuItem} to="StatenIsland"
               onClick={this.props.loadMenu}
               onMouseEnter={this.hoverEnter}
               onMouseLeave={this.hoverLeave}>
