b52a1df799780ee239e3459c08ee86bdf8cf337a
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2019-12-18 13:48:47 -0500

    added 'yearData' class and changed font color to white

--- src/components/PermitsIssued.js
+++ src/components/PermitsIssued.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function PermitsIssued(props) {
   return (
-    <div>
+    <div class="yearData">
       <p>Demolition Permits Issued: {props.demolitionPermits}</p>
       <p>Percent Change from Previous Year: {props.lastYearDemolitionRatio}%</p>
       <p>New Building Permits Issued: {props.newBuildingPermits}</p>
