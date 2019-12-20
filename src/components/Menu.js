d231e832b9b648c262bd4cfb0878c413c1ba85ac
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2019-12-20 14:16:46 -0500

    added homepage blurb

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -49,6 +49,9 @@ class Menu extends Component {
               <li id="Staten-Island">Staten Island</li></Link>
           </ul>
         }
+        {this.props.onHome &&
+          <p className="homePageBlurb">Evolving NYC aims to track reconstruction plans in the city by using the NYC Department of Buildings permit database. It shows how many Demolition permits and "New Building" permits were issued in the last 19 years, and depict this data by year. The ultimate goal is to create a tool that organically tracks plans for development in the city so residents can see projected changes in their community. </p>
+        }
       </menu >
     )
   }
