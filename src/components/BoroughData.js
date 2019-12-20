d38f60e88bf0e9f29b6b4053177c00379efa0485
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2019-12-19 19:20:43 -0500

    updated BoroughData component to close data when another year is clicked and open the new year

--- src/components/BoroughData.js
+++ src/components/BoroughData.js
@@ -15,60 +15,62 @@ class BoroughData extends Component {
       newBuildingPermits: 0,
       lastYearDemolitionRatio: 0,
       lastYearNewBuildingRatio: 0,
-      clickedComment: "",
-      clicked: false,
+      recentlyClickedId: "",
     }
   }
 
   getPermitData = async (year, clickedId) => {
     //Establishing Click Functionality
-    let clicked = this.state.clicked;
-    let clickedComment = this.state.clickedComment;
-    clickedComment = clickedId;
+    let recentlyClickedId = this.state.recentlyClickedId;
+    recentlyClickedId = clickedId;
 
     let borough = document.querySelector(".borough").innerHTML;
     borough = borough.toUpperCase();
 
+
     //Pulling Permit Data
-    if (clicked === false) {
-      clicked = true
-      let demolitionPermits = this.state.demolitionPermits;
-      let newBuildingPermits = this.state.newBuildingPermits;
-      let previousYearDemolitionResponse = await FetchData("DM", borough, year - 1);
-      let previousYearNewBuildingResponse = await FetchData("NB", borough, year - 1);
-      let demolitionResponse = await FetchData("DM", borough, year);
-      let newBuildingResponse = await FetchData("NB", borough, year);
+    let demolitionPermits = this.state.demolitionPermits;
+    let newBuildingPermits = this.state.newBuildingPermits;
+    let previousYearDemolitionResponse = await FetchData("DM", borough, year - 1);
+    let previousYearNewBuildingResponse = await FetchData("NB", borough, year - 1);
+    let demolitionResponse = await FetchData("DM", borough, year);
+    let newBuildingResponse = await FetchData("NB", borough, year);
 
-      demolitionPermits = demolitionResponse.length;
-      newBuildingPermits = newBuildingResponse.length
+    demolitionPermits = demolitionResponse.length;
+    newBuildingPermits = newBuildingResponse.length
 
-      //Calculating Percent Change from Data
-      let percentChange = (A, B) =>
-        Math.round(
-          100 * Math.abs(
-            (A - B) / ((A + B) / 2)
-          )
-        );
-      ;
-      let lastYearDemolitionRatio = percentChange(previousYearDemolitionResponse.length, demolitionPermits);
-      let lastYearNewBuildingRatio = percentChange(previousYearNewBuildingResponse.length, newBuildingPermits);
+    //Calculating Percent Change from Data
+    let percentChange = (A, B) =>
+      Math.round(
+        100 * Math.abs(
+          (A - B) / ((A + B) / 2)
+        )
+      );
+    ;
+    let lastYearDemolitionRatio = percentChange(previousYearDemolitionResponse.length, demolitionPermits);
+    let lastYearNewBuildingRatio = percentChange(previousYearNewBuildingResponse.length, newBuildingPermits);
 
+    if (this.state.recentlyClickedId === clickedId) {
       //Setting State
+      this.setState({
+        demolitionPermits: 0,
+        newBuildingPermits: 0,
+        lastYearDemolitionRatio: 0,
+        lastYearNewBuildingRatio: 0,
+        recentlyClickedId: ""
+      })
+    } else {
       this.setState({
         demolitionPermits,
         newBuildingPermits,
         lastYearDemolitionRatio,
         lastYearNewBuildingRatio,
-        clickedComment,
-        clicked,
-      })
-    } else {
-      clicked = false
-      this.setState({
-        clicked,
+        recentlyClickedId
       })
     }
+
   }
+
 
   render() {
     return (
@@ -77,15 +79,16 @@ class BoroughData extends Component {
           this.props.year.map((year, index) =>
             <div key={index} className="yearContainer">
               <h4 onClick={() => this.getPermitData(year, index)} className="year">{year}</h4>
-              {this.state.clicked && this.state.clickedComment === index ?
-                <PermitsIssued
-                  lastYearDemolitionRatio={this.state.lastYearDemolitionRatio}
-                  lastYearNewBuildingRatio={this.state.lastYearNewBuildingRatio}
-                  demolitionPermits={this.state.demolitionPermits}
-                  newBuildingPermits={this.state.newBuildingPermits}
-                />
-                : null
-              }
+              {/* {this.state.clicked && this.state.recentlyClickedId === index ? */}
+              <PermitsIssued
+                lastYearDemolitionRatio={this.state.lastYearDemolitionRatio}
+                lastYearNewBuildingRatio={this.state.lastYearNewBuildingRatio}
+                demolitionPermits={this.state.demolitionPermits}
+                newBuildingPermits={this.state.newBuildingPermits}
+                shouldOpen={this.state.recentlyClickedId === index ? true : false}
+              />
+              : null
+              {/* } */}
             </div>
           )
         }
--- src/components/PermitsIssued.js
+++ src/components/PermitsIssued.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 function PermitsIssued(props) {
-  return (
-    <div className="yearData">
-      <p><span className="dataType">Demolition Permits:</span> {props.demolitionPermits}</p>
-      <p className="percentData"><span className="dataType">Change from Prior Year:</span> {props.lastYearDemolitionRatio}%</p>
-      <p><span className="dataType">New Building Permits:</span> {props.newBuildingPermits}</p>
-      <p className="percentData"><span className="dataType">Change from Prior Year:</span> {props.lastYearNewBuildingRatio}%</p >
-    </div >
-  )
+  return props.shouldOpen &&
+  // return (
+        <div className="yearData">
+          <p><span className="dataType">Demolition Permits:</span> {props.demolitionPermits}</p>
+          <p className="percentData"><span className="dataType">Change from Prior Year:</span> {props.lastYearDemolitionRatio}%</p>
+          <p><span className="dataType">New Building Permits:</span> {props.newBuildingPermits}</p>
+          <p className="percentData"><span className="dataType">Change from Prior Year:</span> {props.lastYearNewBuildingRatio}%</p >
+        </div >
+  //)
 }
 
 export default PermitsIssued
